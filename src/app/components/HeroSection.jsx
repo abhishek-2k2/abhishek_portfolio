ebe9eb49d5f653cddfcaedd02efe73801cacde53
abhishek-2k2 <123407885+abhishek-2k2@users.noreply.github.com>
2024-10-23 17:46:17 +0530

    second-commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          Passionate Computer Science student with a focus on machine learning, full-stack development, and problem-solving.
+            Passionate Computer Science student with a focus on machine learning,
+            full-stack development, and problem-solving.
           </p>
           <div>
             <Link
@@ -56,19 +57,19 @@ const HeroSection = () => {
             </Link>
           </div>
         </motion.div>
+
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
             <Image
               src="/images/hero-image.jpeg"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="absolute object-cover w-full h-full"
+              layout="fill"
             />
           </div>
         </motion.div>
